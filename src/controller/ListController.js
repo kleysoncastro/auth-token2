9e82fc475e26899dccfbc3c0306a0b20e9afeb71
kleyson castro <kleysoncastro99@gmail.com>
2019-10-12 13:47:59 -0300

    chore: correcao, try chach add em ListController

--- src/controller/ListController.js
+++ src/controller/ListController.js
@@ -2,17 +2,21 @@ const User = require("../model/User");
 
 module.exports = {
   async index(req, res) {
-    const { idUser, firstName } = req.body;
+    try {
+      const { idUser, firstName } = req.body;
 
-    let membro = await User.find(null, "firstName lastName");
+      let membro = await User.find(null, "firstName lastName");
 
-    if (membro) {
-      return res.status(200).send({ membro });
+      if (membro) {
+        return res.status(200).send({ membro });
+      }
+      if (!membro) {
+        res.status(200).send({ erro: "user not fund" });
+      }
+
+      return res.status(200);
+    } catch (err) {
+      res.status(400).send({ erro: "Erro inesperado" });
     }
-    if (!membro) {
-      res.status(200).send({ erro: "nao encontrado" });
-    }
-
-    return res.status(200);
   }
 };
